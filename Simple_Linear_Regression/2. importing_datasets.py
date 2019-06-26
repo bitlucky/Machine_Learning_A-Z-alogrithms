4bd03b313827421e10af72fa6ce4aceed6452552
bitlucky <lokeshtiwari877@gmail.com>
2019-06-26 09:22:42 +0530

    Importing Dataset

---
+++ Simple_Linear_Regression/2. importing_datasets.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
