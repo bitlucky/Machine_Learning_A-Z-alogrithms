4b341692f07d032dedd905383d5400bfd775d070
bitlucky <lokeshtiwari877@gmail.com>
2019-06-26 09:23:44 +0530

    splitting test train

---
+++ Simple_Linear_Regression/3. splitting_test_train.py
@@ -0,0 +1,15 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
