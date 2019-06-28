23a42deb52c0f28f3e8f335a1ced0ea084e5811c
bitlucky <lokeshtiwari877@gmail.com>
2019-06-28 23:27:04 +0530

    Multiple Linear Regression

---
+++ 3. Multiple_Linear_Regression/3. Encoding_categorical_data.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
