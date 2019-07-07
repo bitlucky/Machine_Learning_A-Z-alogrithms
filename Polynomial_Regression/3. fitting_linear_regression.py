8e492499a5e5f239545ceb9c4888d24524817ae8
bitlucky <lokeshtiwari877@gmail.com>
2019-07-07 22:46:17 +0530

    Fitting Linear Regression

--- Polynomial_Regression/3. fitting_linear_regression.py
+++ Polynomial_Regression/3. fitting_linear_regression.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
