886650d6d97508be5c1a501357f43396db4e707f
bitlucky <lokeshtiwari877@gmail.com>
2019-07-07 22:45:54 +0530

    Importing Datasets

--- Polynomial_Regression/2. importing_datasets.py
+++ Polynomial_Regression/2. importing_datasets.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
 
